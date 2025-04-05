8b3c0999e7c01fe4f105dc78ffced52231b48749
Andriy <karpus.andrii@proton.me>
2025-04-05 21:45:29 +0200

    Add view model

---
+++ app/src/main/java/com/andriokar/weatherapp/presentation/WeatherState.kt
@@ -0,0 +1,9 @@
+package com.andriokar.weatherapp.presentation
+
+import com.andriokar.weatherapp.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
